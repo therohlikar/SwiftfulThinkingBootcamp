cbb14158a9e6d05ab999e751ea8be14ae3540eb0
RJ <radek.jenik36@gmail.com>
2022-11-24 14:58:51 +0100

    sheetsbootcamp

---
+++ SheetsBootcamp.swift
@@ -0,0 +1,64 @@
+//
+//  SheetsBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Radek Jeník on 11/24/22.
+//
+
+import SwiftUI
+
+struct SheetsBootcamp: View {
+    @State var showSheet: Bool = false
+    
+    var body: some View {
+        ZStack{
+            Color.green
+                .edgesIgnoringSafeArea(.all)
+            
+            Button {
+                showSheet.toggle()
+            } label: {
+                Text("Second screen")
+                    .foregroundColor(.green)
+                    .padding(20)
+                    .font(.headline)
+                    .background(Color.white.cornerRadius(10))
+            }
+            .fullScreenCover(isPresented: $showSheet, content: {
+                SecondScreen()
+            })
+//            .sheet(isPresented: $showSheet) {
+//                SecondScreen()
+//            }
+
+        }
+    }
+}
+
+struct SecondScreen: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View{
+        ZStack(alignment: .topTrailing){
+            Color.red
+                .edgesIgnoringSafeArea(.all)
+            
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "xmark")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding(20)
+            }
+
+        }
+    }
+}
+
+struct SheetsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        SheetsBootcamp()
+    }
+}
